c686f25ae890689ecb2298dfa0fa9c97279e6804
Ramabele, Kgothalo (LNG-JHB) <ramabelk@legal.regn.net>
2025-01-16 12:16:05 +0200

    generating fixes

--- client/generate.constants.js
+++ client/generate.constants.js
@@ -1,21 +1,21 @@
-import { promises as fs } from 'fs';
-import path from 'path';
-import { Parser } from 'xml2js';
+const fs = require('fs').promises;
+const path = require('path');
+const xml2js = require('xml2js');
 
-const resxFilePath = path.join(__dirname, 'src/display-text/resources.resx');
+const resxFilePath = path.join(__dirname, 'src/resources/resources.resx');
 const outputFilePath = path.join(__dirname, 'src/constants/constants.ts');
 
-const parser = new Parser();
+const parser = new xml2js.Parser();
 
-const readResxFile = async (filePath) => {
+async function readResxFile(filePath) {
   try {
     return await fs.readFile(filePath, 'utf8');
   } catch (error) {
     throw new Error(`Error reading resx file: ${error.message}`);
   }
-};
+}
 
-const parseResxData = (data) => {
+async function parseResxData(data) {
   return new Promise((resolve, reject) => {
     parser.parseString(data, (err, result) => {
       if (err) {
@@ -25,9 +25,9 @@ const parseResxData = (data) => {
       }
     });
   });
-};
+}
 
-const generateConstants = (result) => {
+function generateConstants(result) {
   return result.root.data.reduce((acc, item) => {
     const key = item.$.name.replace('display_text_', '')
       .split('_')
@@ -37,24 +37,24 @@ const generateConstants = (result) => {
     acc[key] = { name: item.$.name, value };
     return acc;
   }, {});
-};
+}
 
-const createConstantsContent = (constants) => {
+function createConstantsContent(constants) {
   return `export enum DisplayConstants {
-    ${Object.entries(constants).map(([key, { name, value }]) => `  /** @constant ${name} */\n  ${key} = "${value}",`).join('\n')}
-    }`;
-};
+${Object.entries(constants).map(([key, { name, value }]) => `  /** @constant ${name} */\n  ${key} = "${value}",`).join('\n\n')}
+}`;
+}
 
-const writeConstantsFile = async (filePath, content) => {
+async function writeConstantsFile(filePath, content) {
   try {
     await fs.writeFile(filePath, content, 'utf8');
     console.log('Constants file generated successfully.');
   } catch (error) {
     throw new Error(`Error writing constants file: ${error.message}`);
   }
-};
+}
 
-const main = async () => {
+async function main() {
   try {
     const data = await readResxFile(resxFilePath);
     const result = await parseResxData(data);
@@ -64,6 +64,6 @@ const main = async () => {
   } catch (error) {
     console.error(error.message);
   }
-};
+}
 
 main();
