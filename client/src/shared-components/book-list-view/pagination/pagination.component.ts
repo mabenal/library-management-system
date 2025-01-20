791f6b20a6c0ff9bc26699ede64db060d969296a
RaboRatona11 <ratonak@legal.regn.net>
2025-01-20 17:42:13 +0200

    adding pagination
    adding pagination but still need some improvement

---
+++ client/src/shared-components/book-list-view/pagination/pagination.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination',
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.less']
+})
+export class PaginationComponent {
+  @Input() currentPage: number = 1;
+  @Input() totalPages: number = 3;
+  @Output() pageChange = new EventEmitter<number>();
+
+  changePage(page: number) {
+    if (page >= 1 && page <= this.totalPages) {
+      this.pageChange.emit(page);
+    }
+  }
+}
