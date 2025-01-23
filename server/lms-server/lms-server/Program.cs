8516066f57686d6450d4c681e446378ec558a0b3
Ramabele, Kgothalo (LNG-JHB) <ramabelk@legal.regn.net>
2025-01-23 16:13:07 +0200

    config manager

--- server/lms-server/lms-server/Program.cs
+++ server/lms-server/lms-server/Program.cs
@@ -105,7 +105,7 @@ builder.Services.AddAuthentication(options =>
         ValidateIssuerSigningKey = true,
         ValidIssuer = builder.Configuration["Jwt:Issuer"],
         ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"] ?? throw new ArgumentNullException("Jwt:Key")))
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
     };
 });
 builder.Services.AddAuthorization();
